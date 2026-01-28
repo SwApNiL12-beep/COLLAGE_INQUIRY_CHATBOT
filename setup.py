cd6d4c2a23404ce4f536e94bc9845b12da8da3a9
SwApNiL12-beep <swapnilshrivas711@gmail.com>
2026-01-28 11:42:47 +0530

    commit changes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup 
+
+setup (
+    name =" Collage_Inquiry_Chatbot",
+    versio="0.1.0",
+    author=" Swapnil",
+    author_email="swapnilshrivas711@gmail.com",
+    packages=find_packages(),
+    install_requires=[] ,
+    
+    
+    
+    )
