945a743cd443cc6c5a48ccc5af6eb308236d895d
SwApNiL12-beep <swapnilshrivas711@gmail.com>
2026-03-02 20:38:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="EQBOT2",
+    version="0.1.0",
+    author="Swapnil Shrivas",
+    author_email="Swapnilshrivas711@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
